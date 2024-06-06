c013c0d9fac29229f390366b9248e946796953b9
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-06 20:54:55 +0300

    base request in the base DAO was created

---
+++ app/dao/base.py
@@ -0,0 +1,130 @@
+from sqlalchemy import delete, insert, select
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.db import async_session_maker
+from app.logger import logger
+
+
+class BaseDAO:
+    model = None
+
+    @classmethod
+    async def find_by_id(cls, model_id: int):
+        try:
+            async with async_session_maker() as session:
+                query = select(cls.model.__table__).filter_by(id=model_id)
+                result = await session.execute(query)
+                return result.scalar_one_or_none()
+        except (SQLAlchemyError, Exception) as error:
+            if isinstance(error, SQLAlchemyError):
+                msg = "Database Exc"
+            if isinstance(error, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot find by id"
+            extra = {
+                "model": cls.model,
+                "model_id": model_id
+            }
+            logger.error(msg, extra=extra, exc_info=True)
+            return {"error": error.__str__()}
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        try:
+            async with async_session_maker() as session:
+                query = select(cls.model.__table__).filter_by(**filter_by)
+                result = await session.execute(query)
+                return result.mappings().first()
+        except (SQLAlchemyError, Exception) as error:
+            if isinstance(error, SQLAlchemyError):
+                msg = "Database Exc"
+            if isinstance(error, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot find one or none by filter"
+            extra = {
+                "model": cls.model,
+                "filter_by": filter_by
+            }
+            logger.error(msg, extra=extra, exc_info=True)
+            return {"error": error.__str__()}
+
+    @classmethod
+    async def get_all(cls, **filter_by):
+        try:
+            async with async_session_maker() as session:
+                query = select(cls.model.__table__).filter_by(**filter_by)
+                result = await session.execute(query)
+                result = result.mappings().all()
+                return result
+        except (SQLAlchemyError, Exception) as error:
+            if isinstance(error, SQLAlchemyError):
+                msg = "Database Exc"
+            if isinstance(error, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot get all objects by filter"
+            extra = {
+                "model": cls.model,
+                "filter_by": filter_by
+            }
+            logger.error(msg, extra=extra, exc_info=True)
+            return {"error": error.__str__()}
+
+    @classmethod
+    async def add(cls, **data):
+        try:
+            async with async_session_maker() as session:
+                query = insert(cls.model).values(**data)
+                await session.execute(query)
+                await session.commit()
+        except (SQLAlchemyError, Exception) as error:
+            if isinstance(error, SQLAlchemyError):
+                msg = "Database Exc"
+            if isinstance(error, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot add object"
+            extra = {
+                "model": cls.model,
+                "data": data
+            }
+            logger.error(msg, extra=extra, exc_info=True)
+            return {"error": error.__str__()}
+
+    @classmethod
+    async def delete(cls, **filter_by):
+        try:
+            async with async_session_maker() as session:
+                query = delete(cls.model).filter_by(**filter_by)
+                result = await session.execute(query)
+                await session.commit()
+                return result.rowcount
+        except (SQLAlchemyError, Exception) as error:
+            if isinstance(error, SQLAlchemyError):
+                msg = "Database Exc"
+            if isinstance(error, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot delete object by filter"
+            extra = {
+                "model": cls.model,
+                "filter_by": filter_by
+            }
+            logger.error(msg, extra=extra, exc_info=True)
+            return {"error": error.__str__()}
+
+    @classmethod
+    async def add_bulk(cls, *data):
+        # Для загрузки массива данных [{"id": 1}, {"id": 2}]
+        try:
+            query = insert(cls.model).values(*data).returning(cls.model.id)
+            async with async_session_maker() as session:
+                result = await session.execute(query)
+                await session.commit()
+                return result.mappings().first()
+        except (SQLAlchemyError, Exception) as e:
+            if isinstance(e, SQLAlchemyError):
+                msg = "Database Exc"
+            elif isinstance(e, Exception):
+                msg = "Unknown Exc"
+            msg += ": Cannot bulk insert data into table"
+
+            logger.error(msg, extra={"table": cls.model.__tablename__}, exc_info=True)
+            return None
