b90a8da45afe8eba0ee0ace2ffdc0f8eb50286cf
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-06 20:09:01 +0300

    added logger

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from time import time
+from fastapi import FastAPI, Request
+from app.houses.router_house import router as router_houses
+from app.logger import logger
+
 
 app = FastAPI()
+app.include_router(router_houses)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time()
+    response = await call_next(request)
+    process_time = time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info("Request execution time", extra={
+        "process_time": round(process_time, 4)
+    })
+    return response
 
 
 @app.get("/")
